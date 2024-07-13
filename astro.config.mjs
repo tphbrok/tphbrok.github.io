2c8a8fd4794057f2469c9ce5727bd0cccd66549e
Thomas Brok <tphbrok@gmail.com>
2024-07-13 17:32:16 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { shield } from "@kindspells/astro-shield";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), shield({})],
-    site: "https://tphbrok.github.io",
+  integrations: [tailwind(), mdx(), shield({}), sitemap()],
+  site: "https://tphbrok.github.io"
 });
